8191683293b225c4a87a4705e72f983d0df14b2a
Dmitry Kireev <dmitry@kireev.co>
2015-12-03 15:11:28 -0800

    add /mingw/bin/autoconf target instead of local one

---
+++ env/macos_10.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+## Disable root check, as brew doesn't need root. Xcode will ask for a sudo password though.
+# if [ "$(id -u)" != "0" ]; then
+#         echo "Sorry, you are not root."
+#         exit 1
+# fi
+
+
+cd ~/
+if [ -d $(eval 'xcode-select -p') ]; then
+  echo "Xcode is installed"
+else
+  echo "Installing Xcode"
+  curl -L -o xcode-6.2-cmd-tools.pkg "https://www.dropbox.com/s/1t7ux39sccxqfwx/xcode-6.2-cmd-tools.pkg\?dl\=0"
+  sudo installer -pkg ~/xcode-6.2-cmd-tools.pkg -target / && rm -rf xcode-6.2-cmd-tools.pkg
+fi
+
+echo "Install Homebrew"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Installing additional tools"
+brew install homebrew/dupes
+brew install binutils coreutils automake wget gawk libtool gperf grep python python3 wget tmux
+brew install gnu-sed --with-default-names
+brew install findutils --with-default-names
+
+# brew install binutils
+# brew install diffutils
+# brew install ed --default-names
+# brew install gawk
+# brew install gnu-indent --with-default-names
+# brew install gnu-sed --with-default-names
+# brew install gnu-tar --with-default-names
+# brew install gnu-which --with-default-names
+# brew install gnutls
+# brew install grep --with-default-names
+# brew install gzip
+# brew install screen
+# brew install watch
+# brew install wdiff --with-gettext
+# brew install wget
