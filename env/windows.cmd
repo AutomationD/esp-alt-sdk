19a217e86b2aa8cd11b1e9ec75e64043adb51c89
Dmitry Kireev <dmitry@kireev.co>
2016-07-31 20:38:57 -0700

    Update test

--- env/windows.cmd
+++ env/windows.cmd
@@ -2,5 +2,7 @@
 choco install babun -y
 call %USERPROFILE%\.babun\cygwin\bin\bash -i -c "pact install zip gawk xz bzip2 tar make wget bsdtar gcc-g++ gettext bison flex byacc automake1.15 autoconf2.5 diffutils texinfo libtool libncursres-devel libintl-devel patch ncurses-devel help2man"
 
+set PYTHONPATH=c:\Users\Administrator\.babun\cygwin\lib\python2.7\
 call %USERPROFILE%\.babun\babun.bat
 pause Please run `make STANDALONE=y` in the shell that has just opened.
+
